51081977aca0b6e27196f59010082b2e032ea9cc
BhashithaArambepola <barambepola@gmail.com>
2022-11-23 11:29:40 +0530

    change ui components

--- main.js
+++ main.js
@@ -1,40 +1,12 @@
-//
-// $('#carouselExample').on('slide.bs.carousel', function (e) {
-//
-//
-//     var $e = $(e.relatedTarget);
-//     var idx = $e.index();
-//     var itemsPerSlide = 3;
-//     var totalItems = $('.carousel-item').length;
-//
-//     if (idx >= totalItems-(itemsPerSlide-1)) {
-//         var it = itemsPerSlide - (totalItems - idx);
-//         for (var i=0; i<it; i++) {
-//             // append slides to end
-//             if (e.direction=="left") {
-//                 $('.carousel-item').eq(i).appendTo('.carousel-inner');
-//             }
-//             else {
-//                 $('.carousel-item').eq(0).appendTo('.carousel-inner');
-//             }
-//         }
-//     }
-// });
-//
-//
-//
-//
-//
-// $(document).ready(function() {
-//     /* show lightbox when clicking a thumbnail */
-//     $('a.thumb').click(function(event){
-//         event.preventDefault();
-//         var content = $('.modal-body');
-//         content.empty();
-//         var title = $(this).attr("title");
-//         $('.modal-title').html(title);
-//         content.html($(this).html());
-//         $(".modal-profile").modal({show:true});
-//     });
-//
-// });
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
