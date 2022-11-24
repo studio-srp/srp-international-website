b09bd4d170f4d969ad31202fa62187e5fcdba6da
BhashithaArambepola <barambepola@gmail.com>
2022-11-24 14:58:08 +0530

    change uni components

--- main.js
+++ main.js
@@ -10,3 +10,22 @@ $('.count').each(function () {
     });
 });
 <!--text animation effect-->
+<script>
+    var global-services = document.getElementById("global-services-h1")
+    /*cover*/
+    function scrolListener1(e) {
+    var screenTop1 = document.scrollingElement.scrollTop;
+    var screenBottom1 = screenTop + innerHeight;
+    var coverTop = global-services.getBoundingClientRect().top
+    if (coverTop < screenBottom1 && coverTop < screenTop1) {
+    global-services.classList.remove("hidetext");
+    global-services.classList.add("showtext");
+    global-services.classList.add("animate__flipInX");
+    global-services.children[0].classList.remove("hidetext");
+    global-services.children[0].classList.add("showtext");
+    global-services.children[0].classList.add("animate__flipInX");
+
+}
+}
+    document.onscroll = scrolListener1
+</script>
